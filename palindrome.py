8f8e78dfa92028d5a44d4157d0232c28b639117c
Keerat Kaur Guliani <58954611+KeeratKG@users.noreply.github.com>
2020-12-17 15:30:17 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -26,5 +26,34 @@ class Solution:
         
 # Solution without using str()
 
-
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        r = 2 ** 31
+        minLimit = r * -1 
+        maxLimit = r - 1 
+        
+        status = False  #declares whether the given integer is a palindrome or not, set False by default 
+                
+        if x < 0:
+            status
+            
+        if x >= 0 and x < maxLimit: 
+            num = x # store copy of x 
+            rev = 0 # to store reverse 
+          
+        # will iterate as long as num is not zero
+            while num:   
+                '''
+                1. remove last digit from original no 
+                2. add it to the one's place in the reverse
+                3. truncate the original and repeat the process 
+                '''
+                
+                last = num % 10
+                rev = rev * 10 + last 
+                num = num // 10
+            
+            return x == rev
+                
+       
 
