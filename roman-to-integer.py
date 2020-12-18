27159a907e8317114d79dba1c8209b825150df5b
Keerat Kaur Guliani <58954611+KeeratKG@users.noreply.github.com>
2020-12-18 17:57:20 +0530

    Update roman-to-integer.py

--- roman-to-integer.py
+++ roman-to-integer.py
@@ -56,3 +56,29 @@ class Solution:
             intValue += values[s[i]]
         
         return intValue
+
+    
+ # Solution 3
+
+# Reference dictionary 
+d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+result = 0 
+
+# One character edge case 
+if (len(s) == 1):
+    return (d[s])
+else:
+    for i in range(len(s)-1):
+        now = s[i]
+        nex_t = s[i+1]
+        if (d[now] < d[nex_t]):
+            result = result - d[now]
+        else:
+            result = result + d[now]
+#Add last character value 
+result = result + d[s[len(s)-1]]
+
+return result 
+  
+            
+            
