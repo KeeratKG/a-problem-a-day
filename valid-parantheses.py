43226b24aaa36c6461db2ad77428ccbeac9e6efa
Keerat Kaur Guliani <58954611+KeeratKG@users.noreply.github.com>
2020-12-21 15:33:41 +0530

    Create valid-parantheses.py

---
+++ valid-parantheses.py
@@ -0,0 +1,74 @@
+# 21 Dec 2020
+# Leetcode 
+
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+'''
+
+# Solution 1 
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        '''
+        logic: 
+        - use stack as intermediate data structure 
+        '''
+        
+         # The stack to keep track of opening brackets.
+        stack = []
+
+        # Hash map for keeping track of mappings. This keeps the code very clean.
+        # Also makes adding more types of parenthesis easier
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        # For every bracket in the expression.
+        for char in s:
+
+            # If the character is an closing bracket
+            if char in mapping:
+
+                # Pop the topmost element from the stack, if it is non empty
+                # Otherwise assign a dummy value of '#' to the top_element variable
+                top_element = stack.pop() if stack else '#'
+
+                # The mapping for the opening bracket in our hash and the top
+                # element of the stack don't match, return False
+                if mapping[char] != top_element:
+                    return False
+            else:
+                # We have an opening bracket, simply push it onto the stack.
+                stack.append(char)
+
+        # In the end, if the stack is empty, then we have a valid expression.
+        # The stack won't be empty for cases like ((()
+        return not stack
+ 
+ 
+ 
+# Solution 2 
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        '''
+        logic: 
+        - use stack as intermediate data structure 
+        '''
+        
+        bracket_map = {"(": ")", "[": "]",  "{": "}"}
+        open_par = set(["(", "[", "{"])
+        stack = []
+        for i in s:
+            if i in open_par:
+                stack.append(i)
+            elif stack and i == bracket_map[stack[-1]]:
+                    stack.pop()
+            else:
+                return False
+        return stack == []
+
+ 
