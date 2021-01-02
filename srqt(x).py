a4bcbaf0f66f4d5d565d1c2cb26bc4cc99424c16
Keerat Kaur Guliani <58954611+KeeratKG@users.noreply.github.com>
2021-01-02 15:20:46 +0530

    Create srqt(x).py

---
+++ srqt(x).py
@@ -0,0 +1,13 @@
+# Leetcode 
+# 2/1/2021
+
+'''
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+'''
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return (int(x**0.5))
+        
