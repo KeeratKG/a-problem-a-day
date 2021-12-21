2ce0bb5d630b30408a91fdfbbeaab9952b8a9194
Keerat Kaur Guliani <58954611+KeeratKG@users.noreply.github.com>
2021-12-21 22:15:00 +0530

    Create kthnodefrommiddleLL.py

---
+++ LinkedLists/kthnodefrommiddleLL.py
@@ -0,0 +1,71 @@
+###
+# Given a linked list A of length N and an integer B.
+
+# You need to find the value of the Bth node from the middle towards the beginning of the Linked List A.
+
+# If no such element exists, then return -1.
+###
+
+# brute force 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # brute force 
+
+    def reverseList(self, A):
+        prev = None 
+        cur = A
+        if cur.next is None:
+            return A 
+
+        while(cur is not None):
+            nxt = cur.next 
+            cur.next = prev 
+            prev = cur 
+            cur = nxt 
+
+        self.head = prev 
+        return self.head
+    # @param A : head node of linked list
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        
+        # length of linked list 
+        length = 0 
+        current = A 
+        while current:
+            current = current.next 
+            length += 1
+        # find the middle node, from the last element 
+        if length%2 == 0: 
+            middle = length//2 
+        else: 
+            middle = length//2 + 1 
+        soln = Solution()
+        last = soln.reverseList(A)
+        # print(last.next.val)
+        # # reach till the middle node first 
+        i = 0 
+        while i<middle-1:
+            i += 1 
+            last = last.next 
+
+        # # traverse B nodes further from this node 
+        # print(last.val)
+        if (length-middle) >= B: 
+            j = 0 
+            while j<B:
+                j += 1 
+                last = last.next 
+                # print(last.val)
+
+            ans = last.val 
+            return ans 
+
+        else:
+            return -1 
